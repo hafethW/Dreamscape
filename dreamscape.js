2dd71858cc885886765a55961ed8902ff76b061f
unknown <st769@njit.edu>
2021-01-10 02:43:19 -0500

    index mod, car until waffle needs input

---
+++ dreamscape.js
@@ -0,0 +1,12 @@
+function changeNail(){
+  document.getElementById("nail").src="fixedNail";
+  onclick = true;
+}
+function changeLoc(){
+  window.location.replace("door.html");
+  onclick = true;
+}
+function changeDoor(){
+  document.getElementById("door").src="door1";
+  onmouseover = true;
+}
